03909ef3a7074f1383bfa08d2460b1a9d4d10311
YehorPytomets <yehor.pytomets@teamdev.com>
2020-04-24 19:50:17 +0300

    Scratch NotFoundPage

---
+++ webclient/app/pages/not-found-page.js
@@ -0,0 +1,16 @@
+import Component from "../componets/component.js";
+
+// TODO implement NotFoundPage.
+export default class NotFoundPage extends Component {
+
+    _markup() {
+        return "";
+    }
+
+    _addEventListeners() {
+    }
+
+    _initNestedComponents() {
+        super._initNestedComponents();
+    }
+}
