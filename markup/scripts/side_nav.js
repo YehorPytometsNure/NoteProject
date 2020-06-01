068f81879197117062f5096fe8a9f1b2f2c924b0
YehorPytomets <yehor.pytomets@teamdev.com>
2020-06-02 01:36:50 +0300

    Fix navigation menu markup

--- markup/scripts/side_nav.js
+++ markup/scripts/side_nav.js
@@ -1,7 +1,21 @@
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "250px";
+// }
+
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.transform = "translateX(0)";
+  document.getElementById("mySidenav").style.pointerEvents = "auto";
+  document.getElementById("mySidenav").style.opacity = "1";
 }
 
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
+
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.transform = "translateX(-166px)";
+  setTimeout(() => {
+    document.getElementById("mySidenav").style.opacity = "0";
+  }, 500);
+  document.getElementById("mySidenav").style.pointerEvents = "none";
 }
--- src/main/resources/public/app/componets/notes/navigation-menu/navigation-menu.js
+++ src/main/resources/public/app/componets/notes/navigation-menu/navigation-menu.js
@@ -7,22 +7,44 @@ export default class NavigationMenu extends Component {
   _markup() {
     return `
         <div id="mySidenav" class="sidenav">
-            <a href="javascript:void(0)" class="closebtn" data-type="close-navigation-menu-button">&times;</a>
-            <div class="slide_menu">
-                <p class="slide_menu_header">Menu</p>
-<!--                TODO: make samples. -->
-<!--                <p class="slide_menu_subheader">Samples</p>-->
-<!--                <div class="first_menu">-->
-<!--                    <p class="slide_menu_item">diary</p>-->
-<!--                    <p class="slide_menu_item">calendar</p>-->
-<!--                    <p class="slide_menu_item">to do list</p>-->
-<!--                </div>-->
-                <p class="slide_menu_subheader">Tags</p>
-                <div class="second_menu" data-type="navigation-menu-tags-container"></div>
-                <input class="input" type="text" placeholder="Enter tag name and press Enter" data-type="tag-input">
-                <div class="create_new_tag" data-type="create-new-tag-button">create new tag</div>
-                <p class="bin" data-type="bin-button">Bin</p>
-                <div class="hide_helper">Hide helper</div>
+            <!--<a href="javascript:void(0)" class="closebtn" data-type="close-navigation-menu-button">&times;</a>-->
+            
+            <div class="scrollbar" id="style-scroll">
+            
+                <div class="slide_menu">
+                    <p class="slide_menu_header">Menu</p>
+                    
+                    <img class="arrow_left" src="images/arrow_left.png" alt="arrow_left" data-type="close-navigation-menu-button">
+                    
+                    <p class="slide_menu_subheader">Tags</p>
+                    
+                    <img class="divide_wave" src="images/wave.png" alt="divide_wave">
+                    
+                    <div class="second_menu" data-type="navigation-menu-tags-container"></div>
+                    
+                    <input class="looking-for-note input" type="text" placeholder="Tag Name" data-type="tag-input">
+    <!--                <input class="input" type="text" placeholder="Enter tag name and press Enter" data-type="tag-input">-->
+                    
+                    <div class="second_menu">
+    <!--                <div class="create_new_tag" data-type="create-new-tag-button">create new tag</div>-->
+                    <p id="create_new_tag" class="slide_menu_item" data-type="create-new-tag-button">+ create new
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                    x="0px" y="0px" viewBox="0 0 152.9 43.4" style="enable-background:new 0 0 152.9 43.4;" 
+                    xml:space="preserve">
+          <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/>
+        </svg></p>
+                    </div>
+        
+                    <div class="second_menu">
+<!--                    <p class="bin" data-type="bin-button">Bin</p>-->
+                    <p class="slide_menu_item bin" data-type="bin-button">Bin<svg version="1.1" xmlns="http://www.w3.org/2000/svg" 
+                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 152.9 43.4" 
+                    style="enable-background:new 0 0 152.9 43.4;" xml:space="preserve">
+      <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/>
+    </svg></p>
+                    </div>
+                    <div class="hide_helper">Hide helper</div>
+                </div>
             </div>
         </div>
     `;
@@ -88,11 +110,19 @@ export default class NavigationMenu extends Component {
   }
 
   closeMenu() {
-    this.rootElement.style.width = '0';
+    // this.rootElement.style.width = '0';
+    document.getElementById("mySidenav").style.transform = "translateX(-166px)";
+    setTimeout(() => {
+      document.getElementById("mySidenav").style.opacity = "0";
+    }, 500);
+    document.getElementById("mySidenav").style.pointerEvents = "none";
   }
 
   openMenu() {
-    this.rootElement.style.width = '250px';
+    // this.rootElement.style.width = '250px';
+    document.getElementById("mySidenav").style.transform = "translateX(0)";
+    document.getElementById("mySidenav").style.pointerEvents = "auto";
+    document.getElementById("mySidenav").style.opacity = "1";
   }
 
   hideTagInput() {
